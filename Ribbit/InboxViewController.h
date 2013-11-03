f203344735510f987d1250785bac4c3c681547ba
Tord Asnes <tord.aasnes@gmail.com>
2013-11-03 23:08:36 +0100

    Stage 1 completed. Basics controllers added

---
+++ Ribbit/InboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  InboxViewController.h
+//  Ribbit
+//
+//  Created by Tord Åsnes on 03/11/13.
+//  Copyright (c) 2013 Tord Åsnes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxViewController : UITableViewController
+
+@end
---
+++ Ribbit/InboxViewController.m
@@ -0,0 +1,54 @@
+//
+//  InboxViewController.m
+//  Ribbit
+//
+//  Created by Tord Åsnes on 03/11/13.
+//  Copyright (c) 2013 Tord Åsnes. All rights reserved.
+//
+
+#import "InboxViewController.h"
+
+@interface InboxViewController ()
+
+@end
+
+@implementation InboxViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    [self performSegueWithIdentifier:@"showLogin" sender:self];
+    
+}
+
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+
+@end
